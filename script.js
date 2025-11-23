a4648611f228a5a6e1e177692e14be8618dbc4d3
rayyansidat869-cyber <rayyansidat869@gmail.com>
2025-11-23 13:27:15 +0400

    Update script.js

--- script.js
+++ script.js
@@ -75,17 +75,17 @@ function renderBoard() {
   });
 }
 
-function checkWinner() {
+function checkWinnerFor(player, board) {
   const wins = [
     [0,1,2],[3,4,5],[6,7,8],
     [0,3,6],[1,4,7],[2,5,8],
     [0,4,8],[2,4,6]
   ];
-  for (let [a,b,c] of wins) {
-    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-      return [a, b, c];
-    }
-  }
+  return wins.some(([a, b, c]) =>
+    board[a] === player && board[b] === player && board[c] === player
+  );
+}
+
   return null;
 }
 
