f5d1545a1c4f168733c171b4250e0f194949ea10
rayyansidat869-cyber <rayyansidat869@gmail.com>
2025-11-23 13:48:51 +0400

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,6 @@ function renderBoard() {
   const cells = document.querySelectorAll(".cell");
   board.forEach((val, i) => {
     cells[i].textContent = val || "";
-    if (highlight.includes(i)) cells[i].style.backgroundColor = "gold"; // ✅ one line
   });
 }
 
